6bbaf89a52cb80aa1c149690d8559ad4fe02e9e5
Duk <dhugkar@gmail.com>
2021-10-29 22:53:08 +0530

    Check Employee Presence UC1

--- src/com/bridgelabz/emp/EmployeeWage.java
+++ src/com/bridgelabz/emp/EmployeeWage.java
@@ -3,7 +3,14 @@ package com.bridgelabz.emp;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		int IS_FULL_TIME=1;
+		double empcheck=Math.floor(Math.random()*10)%2;
+		if (IS_FULL_TIME==empcheck) {
+			System.out.println("Employee is present");          
+		}
+		else {
+			System.out.println("Employee is absent");   
+		}
 
 	}
 
