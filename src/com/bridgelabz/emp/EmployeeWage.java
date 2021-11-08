675623304b9cfcc6f3b223ea77ec85e35d6e9d02
Duk <dhugkar@gmail.com>
2021-11-08 18:16:41 +0530

    modified EmployeeWage.java

--- src/com/bridgelabz/emp/EmployeeWage.java
+++ src/com/bridgelabz/emp/EmployeeWage.java
@@ -2,28 +2,29 @@ package com.bridgelabz.emp;
 
 public class EmployeeWage {
 
-	public static final int IS_PART_TIME = 1; 
+	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
-		// Variables
+		int empHrs = 0;
+		int empWage = 0;
 
-		int empHrs = 0; int empWage = 0;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-		// Computation
+		switch (empCheck) {
+		case IS_PART_TIME:
 
-		int empCheck = (int) Math.floor(Math.random()*10)%3;
+			empHrs = 4;
 
-		switch (empCheck) { case IS_PART_TIME:
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
 
-		empHrs = 4;
+			break;
 
-		break; case IS_FULL_TIME: empHrs = 8;
-
-		break;
-
-		default: empHrs = 0;
-
+		default:
+			empHrs = 0;
 
 		}
 
